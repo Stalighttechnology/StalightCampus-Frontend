6dc4633126ab14fad6cd0e08eb229c593edccbe4
Ritesh N <riteshnvisonex@gmail.com>
2026-08-24 19:07:07 +0530

    revert: implement mandatory Google Play In-App Updates in Android native layer

--- android/app/src/main/java/com/stalight/campus/MainActivity.java
+++ android/app/src/main/java/com/stalight/campus/MainActivity.java
@@ -1,38 +1,12 @@
 package com.stalight.campus;
 
 import android.os.Bundle;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.IntentSenderRequest;
-import androidx.activity.result.contract.ActivityResultContracts;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-
-    private InAppUpdateManager inAppUpdateManager;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(CampusGeofencePlugin.class);
         super.onCreate(savedInstanceState);
-
-        ActivityResultLauncher<IntentSenderRequest> updateLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartIntentSenderForResult(),
-                result -> {
-                    if (inAppUpdateManager != null) {
-                        inAppUpdateManager.handleActivityResult(result);
-                    }
-                }
-        );
-
-        inAppUpdateManager = new InAppUpdateManager(this, updateLauncher);
-        inAppUpdateManager.checkForImmediateUpdate();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (inAppUpdateManager != null) {
-            inAppUpdateManager.resumeUpdateIfInProgress();
-        }
     }
 }
